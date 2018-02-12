ecd0c144454938ced246d72cff44f057f04218a8
Victor Hugo <vct.bta@gmail.com>
2018-02-12 12:00:00 -0200

    Recebe as objetos da classe Field na classe principal

--- app/JsonField.php
+++ app/JsonField.php
@@ -2,7 +2,19 @@
 
 namespace VictorHugoBatista\AcfJsonField;
 
+use VictorHugoBatista\AcfJsonField\Contracts\Field;
+
 class JsonField
 {
+    private $fieldBehavior;
 
+    public function __construct(Field $fieldBehavior)
+    {
+        $this->fieldBehavior = $fieldBehavior;
+    }
+
+    public function getField($fieldName, $postId = '')
+    {
+        return $this->fieldBehavior->getField($fieldName, $postId);
+    }
 }
