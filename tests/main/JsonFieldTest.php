985de3bbdf568f5e2181545fcc7d1c707e1c18d9
Victor Hugo <vct.bta@gmail.com>
2018-02-12 14:35:53 -0200

    Adiciona teste se campo vazio é retornado como um array_vazio

--- tests/main/JsonFieldTest.php
+++ tests/main/JsonFieldTest.php
@@ -5,6 +5,18 @@ use VictorHugoBatista\AcfJsonField\Behaviors\FieldMock;
 
 class JsonFieldTest extends \PHPUnit\Framework\TestCase
 {
+    private $jsonField;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->jsonField = new JsonField(new FieldMock([
+            'empty_field' => '',
+            'one_level_field' => '{"test1":"Elgg zanga mzinga ning","test2":"zimbra ebay wesabe spotify, skype"}',
+            'two_levels_field' => '{"test1":"Cotweet ning","test2":"nuvvo reddit twones lala","test4":{"subteste1":"elgg mog hojoki","subteste2":""}} ',
+        ]));
+    }
+
     public function testClassJsonFieldExists()
     {
         $this->assertEquals(true, class_exists(JsonField::class));
@@ -13,5 +25,10 @@ class JsonFieldTest extends \PHPUnit\Framework\TestCase
     public function testClassFieldMockExists()
     {
         $this->assertEquals(true, class_exists(FieldMock::class));
+    }
+
+    public function testJsonFieldGetsContent()
+    {
+        $this->assertEquals([], $this->jsonField->getField('empty_field'));
     }
 }
