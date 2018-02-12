1a5b1765f607425188747bf47426b2591ca44d2c
Victor Hugo <vct.bta@gmail.com>
2018-02-12 14:11:32 -0200

    Testa se as classes JsonField e FieldTest existem

--- tests/main/JsonFieldTest.php
+++ tests/main/JsonFieldTest.php
@@ -1,9 +1,17 @@
 <?php
 
+use VictorHugoBatista\AcfJsonField\JsonField;
+use VictorHugoBatista\AcfJsonField\Behaviors\FieldTest;
+
 class JsonFieldTest extends \PHPUnit\Framework\TestCase
 {
-    public function testClassExists()
+    public function testClassJsonFieldExists()
     {
-        $this->assertEquals(true, true);
+        $this->assertEquals(true, class_exists(JsonField::class));
+    }
+
+    public function testClassFieldTestExists()
+    {
+        $this->assertEquals(true, class_exists(FieldTest::class));
     }
 }
