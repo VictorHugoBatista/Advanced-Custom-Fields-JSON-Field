b4a32c9969145c5d80b082d526eaea6ac7a91ef2
Victor Hugo <vct.bta@gmail.com>
2018-02-13 14:56:25 -0200

    Separa o o evento de carregamento do campo no admin no arquivo acf-field-initializer

---
+++ assets/js/acf-field-initializer.js
@@ -0,0 +1,17 @@
+/**
+ * Chama a função passada por parâmetro ao carregar
+ * um campo do tipo acf_field na administração.
+ * @param initializeFieldCallback
+ */
+export const initializeJsonField = function(initializeFieldCallback) {
+    if( typeof acf.add_action !== 'undefined' ) { // ACF 5
+        acf.add_action('ready_field/type=json_field', initializeFieldCallback);
+        acf.add_action('append_field/type=json_field', initializeFieldCallback);
+    } else { // ACF 4
+        $(document).on('acf/setup_fields', function(e, postbox) {
+            $(postbox).find('.field[data-field_type="json_field"]').each(function(){
+                initializeFieldCallback( $(this) );
+            });
+        });
+    }
+};
