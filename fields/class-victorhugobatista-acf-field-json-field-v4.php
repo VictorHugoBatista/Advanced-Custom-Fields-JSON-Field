975cc0cbf214ff201932fd1c811c44ec5953ad34
Victor Hugo <vct.bta@gmail.com>
2018-02-14 22:38:52 -0200

    Adiciona componente ao campo gerado pelo plugin (acf v4)

--- fields/class-victorhugobatista-acf-field-json-field-v4.php
+++ fields/class-victorhugobatista-acf-field-json-field-v4.php
@@ -125,7 +125,7 @@ class victorhugobatista_acf_field_json_field extends acf_field {
 		// create Field HTML
 		?>
 		<div id="<?php echo $field['key'] ?>" class="json-field-wrapper">
-
+            <json-field></json-field>
 		</div>
 		<?php
 	}
