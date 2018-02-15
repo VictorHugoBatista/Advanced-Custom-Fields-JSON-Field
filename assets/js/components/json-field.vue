99f0ec9c4f1ae47f633e96ddbc39f98c9e509584
Victor Hugo <vct.bta@gmail.com>
2018-02-14 22:38:04 -0200

    Cria componente vazio json-field

---
+++ assets/js/components/json-field.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        field
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "json-field"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
 import {initializeJsonField} from './acf-field-initializer';
 import Vue from 'vue';
+import JsonField from './components/json-field.vue';
+
+Vue.component('json-field', JsonField);
 
 initializeJsonField($field => {
     new Vue({
