e2433195cf7e79f458cf56574ec719e6632c0044
Victor Hugo <vct.bta@gmail.com>
2018-02-24 11:53:58 -0300

    Renomeia as classes do componente json-field-options

--- assets/js/components/json-field-options.vue
+++ assets/js/components/json-field-options.vue
@@ -1,31 +1,31 @@
 <template>
     <div>
         <input type="hidden" :name="field_name" v-model="JSON.stringify(field_data)" />
-        <ul class="json-field-repeater">
-            <li class="json-field-repeater-item" v-for="field, key in field_data">
-                <div class="json-field-repeater-item-form-group">
-                    <div class="json-field-repeater-item-label">
+        <ul class="json-field-repeater-options">
+            <li class="json-field-repeater-options-item" v-for="field, key in field_data">
+                <div class="json-field-repeater-options-item-form-group">
+                    <div class="json-field-repeater-options-item-label">
                         Label do campo
                     </div>
                     <input type="text" required
-                           class="json-field-repeater-item-field"
+                           class="json-field-repeater-options-item-field"
                            placeholder="Label do campo"
                            v-model="field.label"
                     />
                 </div>
-                <div class="json-field-repeater-item-form-group">
-                    <div class="json-field-repeater-item-label">
+                <div class="json-field-repeater-options-item-form-group">
+                    <div class="json-field-repeater-options-item-label">
                         Slug do campo
                     </div>
                     <input type="text" required
-                           class="json-field-repeater-item-field"
+                           class="json-field-repeater-options-item-field"
                            placeholder="Slug do campo"
                            v-model="field.slug"
                     />
                 </div>
-                <div class="json-field-repeater-item-form-group">
+                <div class="json-field-repeater-options-item-form-group">
                     <input type="button" value="Remover"
-                           class="json-field-repeater-item-button json-field-button"
+                           class="json-field-repeater-options-item-button json-field-button"
                            v-on:click="deleteField(key)"
                     />
                 </div>
@@ -67,53 +67,53 @@
 </script>
 
 <style scoped>
-    .json-field-repeater,
-    .json-field-repeater-item,
-    .json-field-repeater-item-field,
+    .json-field-repeater-options,
+    .json-field-repeater-options-item,
+    .json-field-repeater-options-item-field,
     .json-field-button {
         border-radius: 4px;
     }
 
-    .json-field-repeater,
-    .json-field-repeater-item {
+    .json-field-repeater-options,
+    .json-field-repeater-options-item {
         padding: 10px;
     }
 
-    .json-field-repeater-item {
+    .json-field-repeater-options-item {
         background-color: rgba(0, 0, 0, .3);
         clear: both;
         margin-bottom: 10px;
         overflow: auto;
     }
 
-    .json-field-repeater-item-form-group {
+    .json-field-repeater-options-item-form-group {
         margin-bottom: 10px;
         overflow: auto;
     }
 
-    .json-field-repeater-item-form-group:last-child {
+    .json-field-repeater-options-item-form-group:last-child {
         margin-bottom: 0;
     }
 
-    .json-field-repeater-item-label,
-    .json-field-repeater-item-field {
+    .json-field-repeater-options-item-label,
+    .json-field-repeater-options-item-field {
         display: inline-block;
         float: left;
         margin: 0;
     }
 
-    .json-field-repeater-item-label,
-    .json-field-repeater-item-field,
+    .json-field-repeater-options-item-label,
+    .json-field-repeater-options-item-field,
     .json-field-button {
         font-weight: 500;
         letter-spacing: .04em;
     }
 
-    .json-field-repeater-item-label {
+    .json-field-repeater-options-item-label {
         width: 30%;
     }
 
-    .json-field-repeater-item-field {
+    .json-field-repeater-options-item-field {
         border: 1px solid #2295cc;
         padding: 6px 10px !important;
         width: 70% !important;
