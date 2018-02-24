fc2c756d2ad8b9abfb052139cac2f281fa5ad585
Victor Hugo <vct.bta@gmail.com>
2018-02-23 23:06:13 -0300

    Exporta classe AdminJsonFieldInitializer no arquivo acf-field-initializer

--- assets/js/acf-field-initializer.js
+++ assets/js/acf-field-initializer.js
@@ -1,17 +1,23 @@
-/**
- * Chama a função passada por parâmetro ao carregar
- * um campo do tipo acf_field na administração.
- * @param initializeFieldCallback
- */
-export const initializeJsonField = (initializeFieldCallback, $) => {
-    if( typeof acf.add_action !== 'undefined' ) { // ACF 5
-        acf.add_action('ready_field/type=json_field', initializeFieldCallback);
-        acf.add_action('append_field/type=json_field', initializeFieldCallback);
-    } else { // ACF 4
-        $(document).on('acf/setup_fields', (e, postbox) => {
-            $(postbox).find('.field[data-field_type="json_field"]').each((_, field) => {
-                initializeFieldCallback($(field));
-            });
-        });
+export default class AdminJsonFieldInitializer {
+    constructor($) {
+        this.$ = $;
     }
-};
+
+    /**
+     * Chama a função passada por parâmetro ao carregar
+     * um campo do tipo acf_field na administração.
+     * @param initializeFieldCallback
+     */
+    initializeJsonField(initializeFieldCallback) {
+        if( typeof acf.add_action !== 'undefined' ) { // ACF 5
+            acf.add_action('ready_field/type=json_field', initializeFieldCallback);
+            acf.add_action('append_field/type=json_field', initializeFieldCallback);
+        } else { // ACF 4
+            this.$(document).on('acf/setup_fields', (e, postbox) => {
+                this.$(postbox).find('.field[data-field_type="json_field"]').each((_, field) => {
+                    initializeFieldCallback(this.$(field));
+                });
+            });
+        }
+    }
+}
