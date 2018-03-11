3846eb232c45355b23ff882da5b253cd9cc0a53a
Victor Hugo <vct.bta@gmail.com>
2018-03-11 18:56:14 -0300

    Cria o componente subfield-text

---
+++ assets/js/components/subfield-text.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="json-field-repeater-item-label">
+            {{ label }}
+        </div>
+        <input type="text"
+               class="json-field-repeater-item-field"
+               :value="value"
+               v-on:input="this.subfieldChange"
+        />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "subfield-text",
+        props: [
+            'label',
+            'value',
+        ],
+        methods: {
+            subfieldChange() {
+                this.$emit('subfield-changed');
+            },
+        },
+    }
+</script>
